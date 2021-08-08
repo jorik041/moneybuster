436b825f5142951e2dd457b814563fdc82671e4b
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-08-08 19:44:18 -0400

    Migrate AboutActivity to ViewPager2

--- app/src/main/java/net/eneiluj/moneybuster/android/activity/AboutActivity.java
+++ app/src/main/java/net/eneiluj/moneybuster/android/activity/AboutActivity.java
@@ -1,50 +1,38 @@
 package net.eneiluj.moneybuster.android.activity;
 
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.NavUtils;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import net.eneiluj.moneybuster.R;
 import net.eneiluj.moneybuster.android.fragment.about.AboutFragmentContributingTab;
 import net.eneiluj.moneybuster.android.fragment.about.AboutFragmentCreditsTab;
 import net.eneiluj.moneybuster.android.fragment.about.AboutFragmentLicenseTab;
+import net.eneiluj.moneybuster.databinding.ActivityAboutBinding;
 import net.eneiluj.moneybuster.util.ThemeUtils;
 
 public class AboutActivity extends AppCompatActivity {
 
-    ViewPager mViewPager;
-    TabLayout mTabLayout;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = LayoutInflater.from(this).inflate(R.layout.activity_about, null);
-        setContentView(view);
-        Toolbar toolbar = view.findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        mViewPager = findViewById(R.id.pager);
-        mTabLayout = findViewById(R.id.tabs);
-        mTabLayout.setSelectedTabIndicatorColor(ThemeUtils.primaryColor(this));
+        final ActivityAboutBinding binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.aboutToolbar);
 
-        mViewPager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager()));
-        mTabLayout.setupWithViewPager(mViewPager);
+        binding.aboutTabLayout.setSelectedTabIndicatorColor(ThemeUtils.primaryColor(this));
+        final TabsStateAdapter adapter = new TabsStateAdapter(this);
+        binding.aboutViewPager2.setAdapter(adapter);
+        new TabLayoutMediator(binding.aboutTabLayout, binding.aboutViewPager2, (tab, position) ->
+                tab.setText(adapter.getPageTitle(position))
+        ).attach();
     }
 
     @Override
@@ -54,7 +42,7 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 //Toast.makeText(getApplicationContext(),"Back button clicked", Toast.LENGTH_LONG).show();
@@ -64,62 +52,65 @@ public class AboutActivity extends AppCompatActivity {
         return true;
     }
 
-    private class TabsPagerAdapter extends FragmentPagerAdapter {
-        private final int PAGE_COUNT = 3;
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish(); // close this activity as oppose to navigating up
+        return true;
+    }
 
-        public TabsPagerAdapter(FragmentManager fragmentManager) {
-            super(fragmentManager);
+    private static class TabsStateAdapter extends FragmentStateAdapter {
+        private static final int POS_CREDITS = 0;
+        private static final int POS_CONTRIBUTING = 1;
+        private static final int POS_LICENSE = 2;
+        private static final int TOTAL_COUNT = 3;
+
+        TabsStateAdapter(final FragmentActivity fa) {
+            super(fa);
         }
 
         @Override
-        public int getCount() {
-            return PAGE_COUNT;
+        public int getItemCount() {
+            return TOTAL_COUNT;
         }
 
         /**
          * return the right fragment for the given position
          */
+        @NonNull
         @Override
-        public Fragment getItem(int position) {
+        public Fragment createFragment(final int position) {
             switch (position) {
-                case 0:
+                case POS_CREDITS:
                     return new AboutFragmentCreditsTab();
 
-                case 1:
+                case POS_CONTRIBUTING:
                     return new AboutFragmentContributingTab();
 
-                case 2:
+                case POS_LICENSE:
                     return new AboutFragmentLicenseTab();
 
                 default:
-                    return null;
+                    throw new IllegalStateException("Position " + position + " is not supported.");
             }
         }
 
         /**
          * generate title based on given position
          */
-        @Override
-        public CharSequence getPageTitle(int position) {
+        public int getPageTitle(final int position) {
             switch (position) {
-                case 0:
-                    return getString(R.string.about_credits_tab_title);
+                case POS_CREDITS:
+                    return R.string.about_credits_tab_title;
 
-                case 1:
-                    return getString(R.string.about_contribution_tab_title);
+                case POS_CONTRIBUTING:
+                    return R.string.about_contribution_tab_title;
 
-                case 2:
-                    return getString(R.string.about_license_tab_title);
+                case POS_LICENSE:
+                    return R.string.about_license_tab_title;
 
                 default:
-                    return null;
+                    throw new IllegalStateException("Position " + position + " is not supported.");
             }
         }
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        finish(); // close this activity as oppose to navigating up
-        return true;
     }
 }

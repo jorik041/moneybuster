17765889cb440680794e57e97eebfcaff73ecb3e
Julien Veyssier <eneiluj@posteo.net>
2021-10-16 15:16:01 +0200

    refs #77 add option to disable/hide all nextcloud-related stuff
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- app/src/main/java/net/eneiluj/moneybuster/android/activity/BillsListViewActivity.java
+++ app/src/main/java/net/eneiluj/moneybuster/android/activity/BillsListViewActivity.java
@@ -509,6 +509,14 @@ public class BillsListViewActivity extends AppCompatActivity implements ItemAdap
                 GradientDrawable.Orientation.LEFT_RIGHT, colors);
         drawerLayout.findViewById(R.id.drawer_top_layout).setBackground(gradientDrawable);
 
+        // hide nextcloud related stuff if needed
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean showNextcloudSettings = preferences.getBoolean(getString(R.string.pref_key_show_nextcloud_settings), true);
+        if (!showNextcloudSettings) {
+            drawerLayout.findViewById(R.id.configuredAccountLayout).setVisibility(GONE);
+            drawerLayout.findViewById(R.id.launchAccountSwitcher).setVisibility(GONE);
+        }
+
         ImageView logoView = drawerLayout.findViewById(R.id.drawer_logo);
         logoView.setColorFilter(ThemeUtils.primaryColor(this), PorterDuff.Mode.OVERLAY);
 
